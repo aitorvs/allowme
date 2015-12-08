9bb06c2429ec75330c8654c1c73a800673370b45
Aitor Viana <aitorvs@gmail.com>
2015-12-08 17:51:42 +0100

    Demo app added

---
+++ app/src/main/java/com/aitorvs/android/allowme/MainActivity.java
@@ -0,0 +1,49 @@
+package com.aitorvs.android.allowme;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AllowMeActivity {
+
+    private String mPermission = Manifest.permission.READ_CONTACTS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!AllowMe.isPermissionGranted(mPermission)) {
+                    new AllowMe.Builder()
+                            .setPermissions(mPermission)
+                            .setRational("I need the permission for the demo")
+                            .setCallback(new AllowMeCallback() {
+                                @Override
+                                public void onPermissionResult(int i, PermissionResultSet result) {
+                                    if (result.isGranted(mPermission)) {
+                                        Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }).request(69);
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
